63598ec3dba9d73db3707ccfa65c7a91bcfbcdc6
Insaf <carimovinsaf@yandex.com>
2024-08-01 08:54:49 +0300

    программа оценки кино

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 "use strict";
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+console.log(numberOfFilms)
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из просмотренных фильмов?'),
+      b = prompt('На сколько вы оцените его?'),
+      c = prompt('Один из просмотренных фильмов?'),
+      d = prompt('На сколько вы оцените?');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB)
